943b55ca34dd71b4bd9c81bfeb4c12c0406bbeef
Gil Griva <gil.griva@intel.com>
2016-02-27 17:00:04 +0200

    adding AddNewLocation

--- CarRent/Controllers/LocationController.cs
+++ CarRent/Controllers/LocationController.cs
@@ -10,15 +10,27 @@ namespace CarRent.Controllers
     public class LocationController : Controller
     {
         // GET: Location
-        public ActionResult Index()
-        {
-            return View();
-        }
-
         public ActionResult DisplayAllLocation()
         {
             Context con = new Context();
             return View(con.Locations);
+        }
+
+
+        public ActionResult AddLocation()
+        {
+            Location location = new Location();
+            return View(location);
+        }
+        [HttpPost]
+        public ActionResult AddLocation(Location i_location)
+        {
+            using (Context con = new Context())
+            {
+                con.Locations.Add(i_location);
+                con.SaveChanges();
+            }
+            return RedirectToAction("DisplayAllLocation");
         }
 
     }
