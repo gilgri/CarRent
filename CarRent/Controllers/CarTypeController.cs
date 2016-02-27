d3886f7cb90a4e352a19944e9194bd9fa92fe851
Gil Griva <gil.griva@intel.com>
2016-02-27 23:13:47 +0200

    adding carType controller

---
+++ CarRent/Controllers/CarTypeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarRent.Controllers
+{
+    public class CarTypeController : Controller
+    {
+        // GET: CarType
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
