3639492558789841298e848585409562f96aeae5
Gil Griva <gil.griva@intel.com>
2016-03-04 14:49:49 +0200

    addin branchCRTL

---
+++ CarRent/Controllers/BranchsController.cs
@@ -0,0 +1,21 @@
+﻿using CarRent.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarRent.Controllers
+{
+    public class BranchsController : Controller
+    {
+        // GET: Branchs
+        public ActionResult Index()
+        {
+            Context con = new Context();
+            Branch bb = con.Branchs.Where(b => b.Id == 1).First();
+
+            return View(con.Branchs);
+        }
+    }
+}
